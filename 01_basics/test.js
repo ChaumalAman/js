37e1331987c04eb1e3c4306cbc252c00c7752a27
ChaumalAman <113255943+ChaumalAman@users.noreply.github.com>
2024-09-20 16:51:41 +0000

    basics

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,16 @@
+"use strict";// treat js code as newer version
 console.log("hello");
+let name="rahul" //string
+let age=18 //int
+let isLoggedIn =true //boolean
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //null
+ let score="33"
+ let valurInNumber=Number(score) //converted to number
+
+ //"33" => 33
+ //"33abc"=>NaN
+
+ // "" converted to boolean =>false
+ // "fsnjsn" =>true
