2fd587eeee904ea47594c988cb3aa874f47e73ad
ChaumalAman <113255943+ChaumalAman@users.noreply.github.com>
2024-09-21 17:02:00 +0000

    string

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -4,8 +4,8 @@ let name="rahul" //string
 let age=18 //int
 let isLoggedIn =true //boolean
 
-console.log(typeof undefined); //undefined
-console.log(typeof null); //null
+// console.log(typeof undefined); //undefined
+// console.log(typeof null); //null
  let score="33"
  let valurInNumber=Number(score) //converted to number
 
@@ -14,3 +14,21 @@ console.log(typeof null); //null
 
  // "" converted to boolean =>false
  // "fsnjsn" =>true
+
+//  console.log("1"+2+2);
+//  console.log(1+2+"2");
+
+console.log(null>0);// false => comparison operator change null to 0
+console.log(null==0);
+console.log(null>=0);// true => equlity check does not convert it to 0
+
+
+//strict check with datatype ===
+
+//string declaration
+
+const newname =new String('hello');
+
+console.log(newname.length);
+
+ 
