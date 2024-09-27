16dcfea8c0f0ce0b11eda5b04c8bc33d6c121674
ChaumalAman <113255943+ChaumalAman@users.noreply.github.com>
2024-09-27 04:39:44 +0000

    objects

---
+++ 02_basics/objects2.js
@@ -0,0 +1,29 @@
+const obj =new Object()  // Singleton obj
+obj.name="sam"
+obj.isLoggedIn=false
+obj.email="sam@google.com"
+
+// console.log(obj);
+
+const obj2={1:"a",2:"b"}
+const obj3={3:"a",3:"b"}
+
+// const obj4=Object.assign({},obj2,obj3)
+
+const obj4 ={...obj2,...obj3}
+// console.log(obj4);
+
+// console.log(Object.keys(obj));
+
+// console.log(obj.hasOwnProperty('isLoggedIn'))
+
+const course={
+    name:"js",
+    price:"999",
+    courseInstructor:"Hitesh"
+}
+
+const {courseInstructor : Inst} =course //destructuring of obj
+
+console.log(Inst);
+
